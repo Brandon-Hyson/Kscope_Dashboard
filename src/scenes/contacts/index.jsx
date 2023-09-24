27563cce80f8a997c90aba95edc331a288613642
Brandon Hyson <hyson.brandon90@gmail.com>
2023-09-24 11:07:56 -0400

    Second Commit of Kscope_dashboard

--- src/scenes/contacts/index.jsx
+++ src/scenes/contacts/index.jsx
@@ -1,18 +1,18 @@
-import { Box, Typography, useTheme } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
-import { mockDataTeam } from "../../data/mockData";
-import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
-import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import { mockDataContacts } from "../../data/mockData";
+
 import Header from "../../components/Header";
+import { useTheme } from "@mui/material";
 
 const Team = () => {
   const theme= useTheme();
   const colors = tokens(theme.palette.mode);
 
   const columns = [
-    { field: "id", headerName: "ID" },
+    { field: "id", headerName: "ID", flex: 0.5},
+    { field: "registrarId", headerName: "Registrar ID" },
     {
       field: "name",
       headerName: "Name",
@@ -37,39 +37,25 @@ const Team = () => {
       flex: 1,
     },
     {
-      field: "access",
-      headerName: "Access Level",
+      field: "address",
+      headerName: "Address",
       flex: 1,
-      renderCell: ({ row: {access }}) => {
-        return (
-          <Box
-            width="60%"
-            m="0 auto"
-            p="5px"
-            display="flex"
-            justifyContent="center"
-            backgroundColor={
-              access === "admin"
-                ? colors.greenAccent[600]
-                : colors.greenAccent[700]
-            }
-            borderRadius="4px"
-          >
-            {access ==="admin" && <AdminPanelSettingsOutlinedIcon />}
-            {access ==="manager" && <SecurityOutlinedIcon />}
-            {access ==="user" && <LockOpenOutlinedIcon />}
-            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
-              {access}
-            </Typography>
-          </Box>
-        );
-      },
+    },
+    {
+      field: "city",
+      headerName: "City",
+      flex: 1,
+    },
+    {
+      field: "zipCode",
+      headerName: "ZipCode",
+      flex: 1,
     },
   ];
 
   return (
     <Box m="20px">
-      <Header title="TEAM" subtitle="Managing the Team Members" />
+      <Header title="CONTACTS" subtitle="List of Contacts for Future Reference" />
       <Box
         m="40px 0 0 0"height="75vh" sx={{
           "& .MuiDataGrid-root": {
@@ -90,11 +76,15 @@ const Team = () => {
           "& .MuiDataGrid-footerContainer": {
             borderTop: "none",
             backgroundColor: colors.blueAccent[700]
-          }
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`
+          },
         }}>
         <DataGrid 
-          rows={mockDataTeam}
+          rows={mockDataContacts}
           columns={columns}
+          components={{ Toolbar: GridToolbar }}
         />
       </Box>
     </Box>
